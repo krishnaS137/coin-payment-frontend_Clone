f8d965a0719dafe3a3d4b14fae4a7448722e24e5
Krishna Sonar <sonarkrishna137@gmail.com>
2026-02-03 14:41:27 +0530

    Revert "fix: Add images configuration to next.config.ts for unoptimized images"
    This reverts commit df8bdfd74c8f8474307fbe571242f772d2293d04.

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images:{
-    unoptimized:true,
-    domains:[],
-  },
-  assetPrefix: process.env.NEXT_PUBLIC_ASSET_PREFIX || "",
   /* config options here */
 };
 
